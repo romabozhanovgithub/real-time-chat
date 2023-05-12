40346a7ce2e2eb6943b0cf65994d5d776e2e3fc7
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-05-12 19:31:27 +0300

    update main

--- auth/app/main.py
+++ auth/app/main.py
@@ -12,10 +12,11 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 # ROUTES
+
 
 # EVENTS
 @app.on_event("startup")
