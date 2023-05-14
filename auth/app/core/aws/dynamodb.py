30547a39391a0736f8803172cf815ce5f565157e
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-05-14 22:12:43 +0300

    update DynamoDB

--- auth/app/core/aws/dynamodb.py
+++ auth/app/core/aws/dynamodb.py
@@ -1,3 +1,4 @@
+from aioboto3.dynamodb.table import BatchWriter
 from mypy_boto3_dynamodb.service_resource import Table
 
 from app.core.aws.client import AWSClient
@@ -7,6 +8,10 @@ class DynamoDB(AWSClient):
     service_name = "dynamodb"
     table_name: str
     _table: Table | None = None
+    batch_writer: BatchWriter | None = None
+
+    def __init__(self) -> None:
+        self.batch_writer = self.table.batch_writer()
 
     @property
     def table(self) -> Table:
