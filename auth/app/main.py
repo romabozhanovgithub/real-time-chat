3d2b6a5eb15930c02814a8fb54bc442cd0cf9fd1
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-05-26 22:36:25 +0300

    add community package

--- auth/app/main.py
+++ auth/app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from community.core import settings as community_settings
 
 from app.core.settings import settings
 
 app = FastAPI(title=settings.APP_TITLE, debug=settings.DEBUG)
+community_settings(
+    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+    aws_default_region=settings.AWS_DEFAULT_REGION,
+    aws_endpoint_url=settings.AWS_ENDPOINT_URL,
+)
 
 
 # MIDDLWARES
