f7e1a2a270272b693c0db6ad6e9d5a9fc1f3bf60
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-06-01 20:00:05 +0300

    add update method

--- auth/app/repositories/user.py
+++ auth/app/repositories/user.py
@@ -1,19 +1,30 @@
 from boto3.dynamodb.conditions import Key
 from community.repositories.dynamodb import BaseRepository
+from community.core.types import ItemTable
 
 
 class UserRepository(BaseRepository):
     table_name = "users"
     partition_key = "username"
 
-    async def create(self, item: dict) -> dict:
+    async def create(self, item: dict) -> ItemTable:
         return await self.put_item(item)
 
-    async def get_by_username(self, username: str) -> dict:
-        return await self.get_item(username)
+    async def get_by_username(self, username: str) -> ItemTable:
+        return await self.get_item(partition_key=username)
     
-    async def get_by_email(self, email: str) -> dict:
+    async def get_by_email(self, email: str) -> ItemTable:
         response = await self.scan(
             filter_expression=Key("email").eq(email),
         )
         return response[0] if response else {}
+    
+    async def update(
+        self,
+        username: str,
+        **kwargs,
+    ) -> ItemTable:
+        return await self.update_item(
+            partition_key=username,
+            **kwargs,
+        )
