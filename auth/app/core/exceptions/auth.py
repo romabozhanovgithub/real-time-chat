e80c0fdee7bc5cbbbb11e6cb1a3b1e04fe55d851
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-06-14 18:37:02 +0300

    add PasswordsDoNotMatchException

--- auth/app/core/exceptions/auth.py
+++ auth/app/core/exceptions/auth.py
@@ -7,3 +7,11 @@ class InvalidUsernameException(HTTPException):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid username",
         )
+
+
+class PasswordsDoNotMatchException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Passwords do not match",
+        )
