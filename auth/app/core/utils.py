f8ee528a4ecd1a3bb1a529839eb42151445be9c9
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-06-14 18:09:07 +0300

    add validate_username

--- auth/app/core/utils.py
+++ auth/app/core/utils.py
@@ -1,5 +1,10 @@
+import re
 import uuid
 
 
 def generate_uuid() -> str:
     return str(uuid.uuid4())
+
+
+def validate_username(username: str) -> bool:
+    return bool(re.fullmatch(r'^[\w_]*$', username))
