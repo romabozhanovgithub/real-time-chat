5bf91aaa75d1bbf519304cda111b8f8f52beaa27
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-06-28 19:30:03 +0300

    add dynamodb, AWSResource

--- community/aws/__init__.py
+++ community/aws/__init__.py
@@ -1,2 +1,3 @@
 from community.aws.client import AWSClient
-from community.aws.dynamodb import DynamoDB
+from community.aws.dynamodb import DynamoDB, dynamodb
+from community.aws.resource import AWSResource
